c65d3e333196dd0786519bd90822f4f6f23bf39d
palcherin wekesa <palcherin@gmail.com>
2023-09-28 16:41:58 +0300

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import {BrowserRouter, Routes,  Router} from 'react-router-dom'
+
+function App() {
+
+  return (
+    <>
+    <BrowserRouter>
+
+    <Router>
+      <Routes />
+    </Router>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
