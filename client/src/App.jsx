2b2a8e9cdb582b1a9121cb22f8f3f71b1a435181
palcherin wekesa <palcherin@gmail.com>
2023-09-29 14:12:44 +0300

    added navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,25 @@
 import { useState } from 'react'
 import './App.css'
-import {BrowserRouter, Routes,  Router} from 'react-router-dom'
-
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {Navbar} from './Components/Navbar.jsx'
+import {Home} from './Routes/Home.jsx'
+import {Notice} from './Routes/Notice.jsx'
+import {Trackmap} from './Routes/Trackmap.jsx'
+import {Results} from './Routes/Results.jsx'
+import {signuproute} from './Routes/SignUproute.jsx'
+import {Chatroom} from './Routes/Chatroom.jsx'
 function App() {
 
   return (
     <>
-    <BrowserRouter>
-
-    <Router>
-      <Route path='/' />
-      <Route></Route>
+    
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/> } />
+      <Route path='/notice' element={<Notice/> }/>
       
-    </Router>
-    </BrowserRouter>
+    </Routes>
+    
     </>
   )
 }
---
+++ client/src/Routes/SignUproute.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {SignUp} from '../Components/Sign-up.jsx'
+
+export const SignUproute = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
